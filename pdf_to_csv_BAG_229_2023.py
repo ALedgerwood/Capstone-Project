616d922f0f9738f748eed9b8711e47ace194dc7b
Alex Ledgerwood <teaginny@gmail.com>
2023-10-23 16:09:13 -0500

    inital update

---
+++ pdf_to_csv_BAG_229_2023.py
@@ -0,0 +1,11 @@
+###Convert table data in a pdf file to csv file
+import camelot
+
+# Replace 'input.pdf' with the path to your PDF file.
+pdf_file = "BAG-229-2023.pdf"
+
+# Use camelot to extract the table data from the PDF.
+tables = camelot.read_pdf(pdf_file, flavor='lattice', pages='1-end')
+
+# Replace 'output.csv' with the desired CSV file name.
+tables[0].to_csv("BAG-229-2023.csv")
